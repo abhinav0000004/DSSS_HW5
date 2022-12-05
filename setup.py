68e2e3a918e6a415548ce9c47ba6582b842d1e26
Abhinav Jaivishnu Choudhary <abhinavchoudhary100@gmail.com>
2022-12-05 02:51:55 +0100

    Modified setup.py file

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup,find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
-setup(name='snowflake',
-      version='0.1',
-      author='Abhinav Choudhary',
-      author_email='abhinavchoudhary100@gmail.com',
+setup(name="snowflake",
+      version="0.1",
+      author="Abhinav Choudhary",
+      author_email="abhinavchoudhary100@gmail.com",
       packages=find_packages(),
-      install_requires=["numpy","turtle","random"]
-     )
+      install_requires=["numpy", "turtles"] 
+      )
